5737b92129032c0df91ddd9d70df115cd1cb3d7d
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2026-03-18 14:32:52 +0700

    fix/aptis-193: [BUG] Student - Authentication - Sign in

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -37,7 +37,7 @@ const LoginPage = () => {
 
     try {
       const response = await axiosInstance.post('/users/login', {
-        email: values.email.toLowerCase(),
+        email: values.email.trim().toLowerCase(),
         password: values.password
       })
 
